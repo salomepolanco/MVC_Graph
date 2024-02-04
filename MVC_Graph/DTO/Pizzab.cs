6af78834f99f0b7368ff1104af0bad4fd10c1a16
ospolanco <ospolanco@SAL>
2024-02-03 21:57:23 -0500

    Agregar archivos de proyecto.

---
+++ MVC_Graph/DTO/Pizzab.cs
@@ -0,0 +1,16 @@
+﻿namespace AbstractFactoryConsola.Classes
+{
+    //Clase abstracta pizza, en este punto no se conoce que tipo de pizza va a ser.
+    public abstract class Pizzab
+    {
+        protected string _descripcion;
+        //Objeto público que tomará una descripción dependiendo del tipo de pizza que se va a hacer.
+        public object Descripcion
+        {
+            get
+            {
+                return _descripcion;
+            }
+        }
+    }
+}
